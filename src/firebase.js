eff2c1e6dc84e33125010b701985859c5ff8418d
prerak-123 <preerakcontractor@gmail.com>
2022-02-24 12:21:19 +0530

    Init commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBYiAwHOTbMPako05tdmAmQCKDeZwiZQFc",
+  
+    authDomain: "vestigia.firebaseapp.com",
+  
+    projectId: "vestigia",
+  
+    storageBucket: "vestigia.appspot.com",
+  
+    messagingSenderId: "522858903791",
+  
+    appId: "1:522858903791:web:9575770f6cc0fe3ac9c31a",
+  
+    measurementId: "G-8B1D5KG0K8"
+  
+  };
