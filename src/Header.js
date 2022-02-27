ed022eac4d5200e2afc74d9ca56495a08dabf465
prerak-123 <preerakcontractor@gmail.com>
2022-02-27 14:21:41 +0530

    Added Link to Library

--- src/Header.js
+++ src/Header.js
@@ -1,32 +1,36 @@
 import React from 'react'
 import './Header.css'
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from '@mui/icons-material/Search'
+import {Link} from "react-router-dom"
 
 
-function Header(props) {
-  return (
-    <div className='header'>
+class Header extends React.Component{
+  
+  render(){
+    return (
+      <div className='header'>
+        
+          <img src={process.env.PUBLIC_URL+"logo.png"} alt = 'Logo' className='logo' />
+        <Link to='/library'>
+          <button className='header__button'>Library</button>
+        </Link>
+        <div className='search'>
+          <input type={'search'} placeholder='Search' className='search__bar'></input>
+          <SearchIcon className='search__icon'/>
+        </div>
 
-      <img src={process.env.PUBLIC_URL+"logo.png"} alt = 'Logo' className='logo'/>
+        <div className='user'>
+          <button className='header__button user__button' type="button">{this.props.user}</button>  
+        </div>
+        
+        <div className='github'>
+          <a href='https://github.com/prerak-123/vestigia' className='github__link'>GitHub</a>
+        </div>
 
-      <button className='header__button'>Library</button>
-
-      <div className='search'>
-        <input type={'search'} placeholder='Search' className='search__bar'></input>
-        <SearchIcon className='search__icon'/>
-      </div>
-
-      <div className='user'>
-        <button className='header__button user__button' type="button">{props.user}</button>  
       </div>
       
-      <div className='github'>
-        <a href='https://github.com/prerak-123/vestigia' className='github__link'>GitHub</a>
-      </div>
-
-    </div>
-    
-  )
+    )
+  }
 }
 
 export default Header
